78bc1373def71ee896fce6cad077cf6b80d4a472
pastoraldr <casstno@gmail.com>
2020-04-20 15:51:40 -0400

    Update RungeKutta.py

--- RungeKutta.py
+++ RungeKutta.py
@@ -5,10 +5,26 @@ from scipy import interpolate
 ##### f dde model
 ##### u initial value
 ##### h history function
-##### ts time span
-##### k  1/k is the fixed step size
+##### p parameter
+##### t the current time point
+##### delta the step
 
-def rk4(f, u, h, p, ts, k):
+def rk(f, u, h, p, t ,delta):
+  ### the return of f must be an array
+  k1  = delta*f(u, h, p, t)
+  k2 = delta* f(u+0.5*k1, h, p, t+0.5*delta)
+  k3 = delta* f(u+0.5*k2, h, p, t+0.5*delta)
+  k4 = delta* f(u+k3, h, p, t+delta)
+  nu = u+(k1 + k2 + k2 + k3 + k3 + k4) / 6
+  return(nu)
+
+
+
+
+
+def rk_fixed(f, u, h, p, ts, k):
+    ##### fixed step size
+    ##### k  1/k is rougly the step size
     tslot = np.linspace(ts[0], ts[1], round((ts[1] - ts[0]) * k))
     n  = len(tslot)
     uslot = np.zeros( (n, 2) )
@@ -24,10 +40,7 @@ def rk4(f, u, h, p, ts, k):
     for i in range(n-1):
         tt = tslot[i]
         uu = uslot[i]
-        k1 = delta * f(uu, hnew, p, tt)
-        k2 = delta * f(uu + 0.5 * k1, hnew, p, tt+0.5 * delta)
-        k3 = delta * f(uu+0.5 * k2, hnew, p, tt+0.5 * delta)
-        k4 = delta * f(uu+k3, hnew, p, tt+delta)
-        uslot[i+1] = uu + (k1 + k2 + k2 + k3 + k3 + k4) / 6
+        uslot[i+1] = rk(f, uu, hnew, p, tt ,delta)
     return ((tslot, uslot))
+
 
