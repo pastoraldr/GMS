8ca32e20ed39e36fd33b04ba288e287626777cf4
pastoraldr <casstno@gmail.com>
2020-04-20 14:32:53 -0400

    Create RungeKutta.py

---
+++ RungeKutta.py
@@ -0,0 +1,28 @@
+import math
+import numpy as np
+from scipy import interpolate
+
+
+def rk4(f, u, h, p, ts, k):
+    tslot = np.linspace(ts[0], ts[1], round((ts[1] - ts[0]) * k))
+    n  = len(tslot)
+    uslot = np.zeros( (n, 2) )
+    uslot[0] = u
+    delta = tslot[1] - tslot[0]
+    def hnew(subp, subt):
+        if subt >= tslot[0]:
+            idx = np.argmin(abs(tslot - subt))
+            return (uslot[idx])
+        else:
+            return (h(p, subt))
+
+    for i in range(n-1):
+        tt = tslot[i]
+        uu = uslot[i]
+        k1 = delta * f(uu, hnew, p, tt)
+        k2 = delta * f(uu + 0.5 * k1, hnew, p, tt+0.5 * delta)
+        k3 = delta * f(uu+0.5 * k2, hnew, p, tt+0.5 * delta)
+        k4 = delta * f(uu+k3, hnew, p, tt+delta)
+        uslot[i+1] = uu + (k1 + k2 + k2 + k3 + k3 + k4) / 6
+    return ((tslot, uslot))
+
